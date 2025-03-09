e849571dab3b9257e75570ebb738a56f9283fbea
Stew <92617876+Stew-000-1-0-011@users.noreply.github.com>
2025-03-09 18:31:11 +0900

    udb_read_pyの除外ルールを修正

---
+++ usb_read_py/main.py
@@ -0,0 +1,16 @@
+import serial
+
+def main():
+    print("Hello from usb-read-py!")
+
+    ser = serial.Serial("COM6")
+
+    while True:
+        s = input().encode("utf-8")
+        ser.write(s)
+        for i in range(len(s)): print(ser.read())
+
+
+
+if __name__ == "__main__":
+    main()

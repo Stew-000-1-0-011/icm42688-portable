74b29dd145c119d2a0a95212e43632dd64bc6d3a
Stew <92617876+Stew-000-1-0-011@users.noreply.github.com>
2025-03-14 22:09:44 +0900

    雑にデバッグしててぐちゃぐちゃ

--- python_scripts/spi_from_usb.py
+++ python_scripts/spi_from_usb.py
@@ -1,25 +1,47 @@
 import serial
 
+def print_line(ser):
+	print("debug: print_line")
+	head = ser.read(1)
+	if head == b'E':
+		data = []
+		while ser.in_waiting > 0:
+			read = ser.read(1)
+			if read == b'\n':
+				break
+			data.append(read)
+		print(b"".join(data).decode("utf-8"))
+	elif head == b'O':
+		while ser.in_waiting > 0:
+			data = ser.read(1)
+			print(data.hex(), end=" ")
+			if data == b'\n':
+				break
+		print()
+	print("debug: print_line done")
+
 def main():
 	print("Hello from spi_from_usb!")
 
 	ser = serial.Serial("COM6")
 
 	while True:
-		s = input().encode("utf-8").split()
+		s = input().split()
 		assert len(s) == 3
 		rw = s[0]
 		addr = int(s[1], 0)
+		# print(f"debug: rw={rw}, addr={addr}, data={s[2]}")
 
 		if rw == "r":
 			num = int(s[2], 0)
 			first_byte = 0x80 | addr
-			ser.write(bytes([first_byte, num]))
-			for i in range(num): print(ser.read())
+			ser.write(b'r' + bytes([first_byte]) + bytes([0] * (num - 1)))
+			print_line(ser)
+			print_line(ser)
 		elif rw == "w":
 			assert len(s[2]) == 2
 			first_byte = addr
-			data = bytes([first_byte]) + bytes.fromhex(s[2])
+			data = b'w' + bytes([first_byte]) + bytes.fromhex(s[2])
 			ser.write(data)
 		else:
 			print("Invalid command.")
